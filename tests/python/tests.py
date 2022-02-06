f8d6bd8d89d372c000c3e032bc33c63d5af62e83
Lev Kokotov <lev.kokotov@gmail.com>
2022-02-06 11:22:09 -0800

    rename

--- tests/python/tests.py
+++ tests/python/tests.py
@@ -1,11 +1,11 @@
 import psycopg2
 
-conn = psycopg2.connect("postgres://random:password@127.0.0.1:5433/db")
+conn = psycopg2.connect("postgres://random:password@127.0.0.1:6432/db")
 cur = conn.cursor()
 
-cur.execute("SELECT $1", [1234]);
+cur.execute("SELECT 1");
 res = cur.fetchall()
 
 print(res)
 
-conn.commit()
+# conn.commit()
