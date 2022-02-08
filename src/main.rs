75dd88a099967c5ec834d4764bb95b63955ac6ed
Lev Kokotov <levkk@users.noreply.github.com>
2022-02-08 14:59:44 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-//     PgCat, a PostgreSQL pooler with load balancing, failover, and sharding support.
-//     Copyright (C) 2022  Lev Kokotov <lev@levthe.dev>
+// PgCat, a PostgreSQL pooler with load balancing, failover, and sharding support.
+// Copyright (C) 2022  Lev Kokotov <lev@levthe.dev>
 
-//     This program is free software: you can redistribute it and/or modify
-//     it under the terms of the GNU General Public License as published by
-//     the Free Software Foundation, either version 3 of the License, or
-//     (at your option) any later version.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 
-//     This program is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//     GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// NU General Public License for more details.
 
-//     You should have received a copy of the GNU General Public License
-//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 extern crate async_trait;
 extern crate bb8;
 extern crate bytes;
