12011be3ec81fe0c6a43efebd7ab6a9cccd765bf
Lev Kokotov <lev.kokotov@gmail.com>
2022-02-10 11:08:57 -0800

    tests

---
+++ .circleci/run_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+./target/debug/pgcat &
+
+sleep 1
+
+psql -f tests/sharding/query_routing_setup.sql
+
+# Setup PgBench
+pgbench -i -h 127.0.0.1 -p 6432
+
+# Run it
+pgbench -h 127.0.0.1 -p 6432 -t 500 -c 2
+
+psql -h 127.0.0.1 -p 6432 -f tests/sharding/query_routing_test_insert.sql
+
+psql -h 127.0.0.1 -p 6432 -f tests/sharding/query_routing_test_select.sql
+
+# psql -f tests/sharding/query_routing_test_validate.sql
+
+psql -h 127.0.0.1 -p 6432 -f tests/sharding/query_routing_test_primary_replica.sql
