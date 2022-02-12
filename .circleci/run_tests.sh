eb45d651100107a2b5aa830fd671b2d7444afc35
Lev Kokotov <lev.kokotov@gmail.com>
2022-02-11 22:24:27 -0800

    extended protocol tests

--- .circleci/run_tests.sh
+++ .circleci/run_tests.sh
@@ -13,12 +13,16 @@ psql -e -h 127.0.0.1 -p 5432 -U postgres -f tests/sharding/query_routing_setup.s
 pgbench -i -h 127.0.0.1 -p 6432
 
 # Run it
-pgbench -h 127.0.0.1 -p 6432 -t 500 -c 2
+pgbench -h 127.0.0.1 -p 6432 -t 500 -c 2 --protocol simple
 
+# Extended protocol
+pgbench -h 127.0.0.1 -p 6432 -t 500 -c 2 --protocol extended
+
+# Sharding insert
 psql -e -h 127.0.0.1 -p 6432 -f tests/sharding/query_routing_test_insert.sql
 
+# Sharding select
 psql -e -h 127.0.0.1 -p 6432 -f tests/sharding/query_routing_test_select.sql > /dev/null
 
-# psql -f tests/sharding/query_routing_test_validate.sql
-
+# Replica/primary selection & more sharding tests
 psql -e -h 127.0.0.1 -p 6432 -f tests/sharding/query_routing_test_primary_replica.sql > /dev/null
