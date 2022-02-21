42019f7c3870133501eac37c84b05eb8c9df01c9
Lev Kokotov <lev.kokotov@gmail.com>
2022-02-21 00:11:02 -0800

    test session mode and config reload for real

--- .circleci/run_tests.sh
+++ .circleci/run_tests.sh
@@ -34,8 +34,14 @@ psql -e -h 127.0.0.1 -p 6432 -f tests/sharding/query_routing_test_select.sql > /
 # Replica/primary selection & more sharding tests
 psql -e -h 127.0.0.1 -p 6432 -f tests/sharding/query_routing_test_primary_replica.sql > /dev/null
 
-# Test reload config
+# Test session mode (and config reload)
+sed -i 's/pool_mode = "transaction"/pool_mode = "session"/' pgcat.toml
+
+# Reload config
 kill -SIGHUP $(pgrep pgcat)
+
+# Prepared statements that will only work in session mode
+pgbench -h 127.0.0.1 -p 6432 -t 500 -c 2 --protocol prepared
 
 #
 # ActiveRecord tests!
