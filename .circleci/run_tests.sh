c359abf47a2990e0178e7411db32101e3db134ba
Lev Kokotov <lev.kokotov@gmail.com>
2022-02-24 20:40:02 -0800

    cleaner

--- .circleci/run_tests.sh
+++ .circleci/run_tests.sh
@@ -3,26 +3,34 @@
 set -e
 set -o xtrace
 
+# Start PgCat with a particular log level
+# for inspection.
+function start_pgcat() {
+    kill -s SIGINT $(pgrep pgcat) || true
+    RUST_LOG=${1} ./target/debug/pgcat .circleci/pgcat.toml &
+    sleep 1
+}
+
+# Setup the database with shards and user
 psql -e -h 127.0.0.1 -p 5432 -U postgres -f tests/sharding/query_routing_setup.sql
 
-# Toxiproxy
+# Install Toxiproxy to simulate a downed/slow database
 wget -O toxiproxy-2.1.4.deb https://github.com/Shopify/toxiproxy/releases/download/v2.1.4/toxiproxy_2.1.4_amd64.deb
 sudo dpkg -i toxiproxy-2.1.4.deb
-toxiproxy-server &
-sleep 2
-toxiproxy-cli create -l 127.0.0.1:5433 -u 127.0.0.1:5432 postgres_replica
 
-RUST_LOG=info ./target/debug/pgcat .circleci/pgcat.toml &
+# Start Toxiproxy
+toxiproxy-server &
 sleep 1
 
-# Setup PgBench
-pgbench -i -h 127.0.0.1 -p 6432
+# Create a database at port 5433, forward it to Postgres
+toxiproxy-cli create -l 127.0.0.1:5433 -u 127.0.0.1:5432 postgres_replica
 
-# Run it
-pgbench -h 127.0.0.1 -p 6432 -t 500 -c 2 --protocol simple
+start_pgcat "info"
 
-# Extended protocol
-pgbench -h 127.0.0.1 -p 6432 -t 500 -c 2 --protocol extended
+# pgbench test
+pgbench -i -h 127.0.0.1 -p 6432 && \
+    pgbench -h 127.0.0.1 -p 6432 -t 500 -c 2 --protocol simple && \
+    pgbench -h 127.0.0.1 -p 6432 -t 500 -c 2 --protocol extended
 
 # COPY TO STDOUT test
 psql -h 127.0.0.1 -p 6432 -c 'COPY (SELECT * FROM pgbench_accounts LIMIT 15) TO STDOUT;' > /dev/null
@@ -43,17 +51,16 @@ psql -e -h 127.0.0.1 -p 6432 -f tests/sharding/query_routing_test_primary_replic
 #
 # ActiveRecord tests
 #
-cd tests/ruby
-sudo gem install bundler
-bundle install
-ruby tests.rb
-
+cd tests/ruby && \
+    sudo gem install bundler && \
+    bundle install && \
+    ruby tests.rb && \
 cd ../..
-kill -s SIGINT $(pgrep pgcat)
-RUST_LOG=debug ./target/debug/pgcat .circleci/pgcat.toml &
-sleep 1
 
-# Failover tests
+# Start PgCat in debug to demonstrate failover better
+start_pgcat "debug"
+
+# Add latency to the replica at port 5433 slightly above the healthcheck timeout
 toxiproxy-cli toxic add -t latency -a latency=300 postgres_replica
 sleep 1
 
@@ -62,14 +69,15 @@ timeout 5 psql -e -h 127.0.0.1 -p 6432 -c 'SELECT 1' > /dev/null
 timeout 5 psql -e -h 127.0.0.1 -p 6432 -c 'SELECT 1' > /dev/null
 timeout 5 psql -e -h 127.0.0.1 -p 6432 -c 'SELECT 1' > /dev/null
 
-kill -s SIGINT $(pgrep pgcat)
-RUST_LOG=info ./target/debug/pgcat .circleci/pgcat.toml &
-sleep 1
+# Remove latency
+toxiproxy-cli toxic remove --toxicName latency_downstream postgres_replica
+
+start_pgcat "info"
 
 # Test session mode (and config reload)
 sed -i 's/pool_mode = "transaction"/pool_mode = "session"/' pgcat.toml
 
-# Reload config
+# Reload config test
 kill -SIGHUP $(pgrep pgcat)
 
 # Prepared statements that will only work in session mode
