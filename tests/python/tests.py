7f40bb27b9287156214a33ec5506edb2eb1173f0
Mostafa Abdelraouf <mostafa.abdelraouf@instacart.com>
2022-07-31 02:18:00 -0500

    fix python test

--- tests/python/tests.py
+++ tests/python/tests.py
@@ -1,12 +1,11 @@
 import psycopg2
 
 def test_normal_db_access():
-    conn = psycopg2.connect("postgres://random:password@127.0.0.1:6432/db")
+    conn = psycopg2.connect("postgres://sharding_user:sharding_user@127.0.0.1:6432/sharded_db?application_name=testing_pgcat")
     cur = conn.cursor()
 
     cur.execute("SELECT 1")
     res = cur.fetchall()
-
     print(res)
 
 
@@ -18,8 +17,6 @@ def test_admin_db_access():
     cur.execute("SHOW POOLS")
     res = cur.fetchall()
     print(res)
-
-
 
 test_normal_db_access()
 test_admin_db_access()
