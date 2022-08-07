378336f8c41a9832f22908f625573de65e992839
Mostafa Abdelraouf <mostafa.abdelraouf@instacart.com>
2022-08-07 06:35:58 -0500

    quote

--- .circleci/generate_coverage.sh
+++ .circleci/generate_coverage.sh
@@ -2,6 +2,6 @@
 
 rust-profdata merge -sparse pgcat-*.profraw -o pgcat.profdata
 
-rust-cov export -ignore-filename-regex=rustc|registry -Xdemangler=rustfilt -instr-profile=pgcat.profdata --object ./target/debug/pgcat --format lcov > ./lcov.info
+rust-cov export -ignore-filename-regex="rustc|registry" -Xdemangler=rustfilt -instr-profile=pgcat.profdata --object ./target/debug/pgcat --format lcov > ./lcov.info
 
 genhtml lcov.info --output-directory /tmp/cov --prefix $(pwd)
