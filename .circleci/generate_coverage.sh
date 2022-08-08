b79f55abd64a209faa014a910f7350fe4bde4708
Mostafa Abdelraouf <mostafa.abdelraouf@instacart.com>
2022-08-08 09:51:36 -0500

    Generate test coverage report in CircleCI (#110)
    * coverage?
    
    * generate_coverage
    
    * +x
    
    * 1.62.1
    
    * 62
    
    * ignore
    
    * store
    
    * quote

---
+++ .circleci/generate_coverage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rust-profdata merge -sparse pgcat-*.profraw -o pgcat.profdata
+
+rust-cov export -ignore-filename-regex="rustc|registry" -Xdemangler=rustfilt -instr-profile=pgcat.profdata --object ./target/debug/pgcat --format lcov > ./lcov.info
+
+genhtml lcov.info --output-directory /tmp/cov --prefix $(pwd)
