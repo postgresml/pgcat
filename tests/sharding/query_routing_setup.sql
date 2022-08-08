7fc3ac4f28482c48696e0e8f7f52bfdeebcda77e
Mostafa Abdelraouf <mostafa.abdelraouf@instacart.com>
2022-08-08 14:31:29 -0500

    Fix Dev env

--- tests/sharding/query_routing_setup.sql
+++ tests/sharding/query_routing_setup.sql
@@ -1,11 +1,12 @@
-
 DROP DATABASE IF EXISTS shard0;
 DROP DATABASE IF EXISTS shard1;
 DROP DATABASE IF EXISTS shard2;
+DROP DATABASE IF EXISTS some_db
 
 CREATE DATABASE shard0;
 CREATE DATABASE shard1;
 CREATE DATABASE shard2;
+CREATE DATABASE some_db
 
 \c shard0
 
@@ -41,12 +42,25 @@ CREATE TABLE data (
 
 CREATE TABLE data_shard_2 PARTITION OF data FOR VALUES WITH (MODULUS 3, REMAINDER 2);
 
-DROP ROLE IF EXISTS sharding_user;
-CREATE ROLE sharding_user ENCRYPTED PASSWORD 'sharding_user' LOGIN;
 
-GRANT CONNECT ON DATABASE shard0 TO sharding_user;
-GRANT CONNECT ON DATABASE shard1 TO sharding_user;
-GRANT CONNECT ON DATABASE shard2 TO sharding_user;
+\c some_db
+
+DROP TABLE IF EXISTS data CASCADE;
+
+CREATE TABLE data (
+    id BIGINT,
+    value VARCHAR
+);
+
+DROP ROLE IF EXISTS sharding_user;
+DROP ROLE IF EXISTS simple_user;
+CREATE ROLE sharding_user ENCRYPTED PASSWORD 'sharding_user' LOGIN;
+CREATE ROLE simple_user ENCRYPTED PASSWORD 'simple_user' LOGIN;
+
+GRANT CONNECT ON DATABASE shard0  TO sharding_user;
+GRANT CONNECT ON DATABASE shard1  TO sharding_user;
+GRANT CONNECT ON DATABASE shard2  TO sharding_user;
+GRANT CONNECT ON DATABASE some_db TO simple_user;
 
 \c shard0
 GRANT ALL ON SCHEMA public TO sharding_user;
@@ -59,3 +73,7 @@ GRANT ALL ON TABLE data TO sharding_user;
 \c shard2
 GRANT ALL ON SCHEMA public TO sharding_user;
 GRANT ALL ON TABLE data TO sharding_user;
+
+\c some_db
+GRANT ALL ON SCHEMA public TO simple_user;
+GRANT ALL ON TABLE data TO simple_user;
