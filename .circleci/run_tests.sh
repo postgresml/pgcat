335ad621ace7e32f16f8a7c6acdf8c551c71bec6
Zain Kabani <zain.kabani@instacart.com>
2022-08-08 14:32:54 -0400

    Update python test bash script ordering

--- .circleci/run_tests.sh
+++ .circleci/run_tests.sh
@@ -76,8 +76,8 @@ cd ../..
 # Python tests
 # These tests will start and stop the pgcat server so it will need to be restarted after the tests
 #
-pip3 install -r tests/python/requirements.txt && \
-    python3 tests/python/tests.py
+pip3 install -r tests/python/requirements.txt
+python3 tests/python/tests.py
 
 start_pgcat "info"
 
