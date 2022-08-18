5948fef6cf97bc9ca7c720f89f63793a22705f4f
zainkabani <77307340+zainkabani@users.noreply.github.com>
2022-08-18 08:12:38 -0400

    Minor Refactoring of re-used code and server stat reporting (#129)
    * Minor changes to stats reporting and recduce re-used code
    
    * fmt

--- src/pool.rs
+++ src/pool.rs
@@ -335,7 +335,7 @@ impl ConnectionPool {
             if !require_healthcheck {
                 self.stats
                     .checkout_time(now.elapsed().as_micros(), process_id, address.id);
-                self.stats.server_idle(conn.process_id(), address.id);
+                self.stats.server_active(conn.process_id(), address.id);
                 return Ok((conn, address.clone()));
             }
 
@@ -354,7 +354,7 @@ impl ConnectionPool {
                     Ok(_) => {
                         self.stats
                             .checkout_time(now.elapsed().as_micros(), process_id, address.id);
-                        self.stats.server_idle(conn.process_id(), address.id);
+                        self.stats.server_active(conn.process_id(), address.id);
                         return Ok((conn, address.clone()));
                     }
 
