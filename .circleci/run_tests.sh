1de08e02cc36c2daac67dd6814a07bdb14b959e8
Mostafa Abdelraouf <mostafa.abdelraouf@instacart.com>
2022-09-04 20:19:47 -0500

    fix

--- .circleci/run_tests.sh
+++ .circleci/run_tests.sh
@@ -38,7 +38,7 @@ toxiproxy-cli create -l 127.0.0.1:5433 -u 127.0.0.1:5432 postgres_replica
 start_pgcat "info"
 
 # Test number of clients
-PGPASSWORD=sharding_user pgbench -h 127.0.0.1 -p 6432 -U sharding_user sharded_db -c 16000
+PGPASSWORD=sharding_user pgbench -h 127.0.0.1 -p 6432 -U sharding_user sharded_db -c 4096
 
 # Check that prometheus is running
 curl --fail localhost:9930/metrics
