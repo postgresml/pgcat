9b1d3e1c3deacdd76daefb03ffc5578020a72a0b
Mostafa Abdelraouf <mostafa.abdelraouf@instacart.com>
2023-02-15 13:27:33 -0600

    Enable more auth methods in CI

--- tests/docker/run.sh
+++ tests/docker/run.sh
@@ -4,6 +4,9 @@ rm /app/*.profraw || true
 rm /app/pgcat.profdata || true
 rm -rf /app/cov || true
 
+export RUSTFLAGS="-C instrument-coverage"
+export LLVM_PROFILE_FILE="pgcat-%m.profraw"
+
 cd /app/
 
 cargo build
@@ -11,9 +14,16 @@ cargo test --tests
 
 bash .circleci/run_tests.sh
 
+TEST_OBJECTS=$( \
+    for file in $(cargo test --no-run 2>&1 | grep "target/debug/deps/pgcat-[[:alnum:]]\+" -o); \
+    do \
+        printf "%s %s " --object $file; \
+    done \
+)
+
 rust-profdata merge -sparse pgcat-*.profraw -o pgcat.profdata
 
-rust-cov export -ignore-filename-regex="rustc|registry" -Xdemangler=rustfilt -instr-profile=pgcat.profdata --object ./target/debug/pgcat --format lcov > ./lcov.info
+bash -c "rust-cov export -ignore-filename-regex='rustc|registry' -Xdemangler=rustfilt -instr-profile=pgcat.profdata $TEST_OBJECTS --object ./target/debug/pgcat --format lcov > ./lcov.info"
 
 genhtml lcov.info --output-directory cov --prefix $(pwd)
 
