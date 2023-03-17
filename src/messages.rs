ea6518ea1a863a2b9843c8b616d3ee5822855db8
Mostafa Abdelraouf <mostafa.abdelraouf@instacart.com>
2023-03-17 11:44:30 -0500

    Check Slice bounds in read_message to avoid panics

--- src/messages.rs
+++ src/messages.rs
@@ -517,12 +517,19 @@ where
 
     bytes.resize(bytes.len() + len as usize - mem::size_of::<i32>(), b'0');
 
+    let slice_start = mem::size_of::<u8>() + mem::size_of::<i32>();
+    let slice_end = slice_start + len as usize - mem::size_of::<i32>();
+
+    // Avoids a panic
+    if slice_end < slice_start {
+        return Err(Error::SocketError(format!(
+            "Error reading message from socket - Code: {:?} - Length {:?}, Error: {:?}",
+            code, len, "Unexpected length value for message"
+        )));
+    }
+
     match stream
-        .read_exact(
-            &mut bytes[mem::size_of::<u8>() + mem::size_of::<i32>()
-                ..mem::size_of::<u8>() + mem::size_of::<i32>() + len as usize
-                    - mem::size_of::<i32>()],
-        )
+        .read_exact(&mut bytes[slice_start..slice_end])
         .await
     {
         Ok(_) => (),
