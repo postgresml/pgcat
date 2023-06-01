9b6b6c5cf5191353792c7875f0b4221fd6259996
Zain Kabani <zain.kabani@instacart.com>
2023-05-31 22:10:16 -0400

    Add pgcat start to python test

--- src/config.rs
+++ src/config.rs
@@ -355,7 +355,7 @@ impl General {
     }
 
     pub fn default_idle_timeout() -> u64 {
-        60000 // 10 minutes
+        600000 // 10 minutes
     }
 
     pub fn default_shutdown_timeout() -> u64 {
