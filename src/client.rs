4169b47ff134298b61cb9ada07522e405f195eed
Zain Kabani <zain.kabani@instacart.com>
2023-08-09 15:10:48 -0400

    fix

--- src/client.rs
+++ src/client.rs
@@ -778,7 +778,7 @@ where
         let mut will_prepare = false;
 
         let client_identifier =
-            ClientIdentifier::new(&self.application_name, &self.username, &self.pool_name);
+            ClientIdentifier::new(&self.server_parameters.get_application_name(), &self.username, &self.pool_name);
 
         // Our custom protocol loop.
         // We expect the client to either start a transaction with regular queries
